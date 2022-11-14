b992a77a3a49e62acc5613a76ea90a71021929ec
jhyunlee94 <jkwhd12@gmail.com>
2022-11-14 22:54:44 +0900

    Create controllers, routers

--- src/server.js
+++ src/server.js
@@ -3,41 +3,18 @@
 /* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-	console.log(` ${req.method} : ${req.url}`);
-	next();
-};
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-// const privateMiddleware = (req, res, next) => {
-// 	const url = req.url;
-// 	if (url === "/protected") {
-// 		return res.send("<h1>Not Allowed</h1>");
-// 	}
-// 	console.log("Allowed, you may continue. ");
-// 	next();
-// };
-
-const handleHome = (req, res) => {
-	return res.send("I love middleware");
-};
-
-// const handleProtected = (req, res) => {
-// 	return res.send("Welcom to the private lounge.");
-// };
-
-// app.use() : global middlewares
-// app.use(logger);
-// app.use(privateMiddleware);
-
-app.get("/", logger, handleHome);
-// app.get("/protected", handleProtected);
-
-const handleListening = () => {
-	console.log("Server listening on port http://localhost:4000");
-};
-
+const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 app.listen(PORT, handleListening);
