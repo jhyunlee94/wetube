fc30f3a61edbb94b3d6610defda08b990e3919d7
jhyunlee94 <jkwhd12@gmail.com>
2022-11-14 23:56:18 +0900

    4.8 URL Parameters part Two

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,8 +4,9 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+// (\\d+) : 이 다음부터 문장 끝까지 숫자만 받아옴
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
